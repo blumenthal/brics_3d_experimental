cff34c80ba078690bea177d3407f788b4fc6ef22
blumenthal <blumenthal@locomotec.com>
2012-12-05 09:22:25 +0100

    Cleaned up genereated files.

--- brics_3d_msgs/src/brics_3d_msgs/msg/__init__.py
+++
@@ -1,7 +0,0 @@
-from ._SceneObjects import *
-from ._Attribute import *
-from ._PointCloudSegments import *
-from ._Shape import *
-from ._SceneGraphUpdate import *
-from ._Coordination import *
-from ._SceneObject import *
